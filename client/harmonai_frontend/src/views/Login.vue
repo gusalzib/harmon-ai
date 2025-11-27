d0ded8fb8e9eacfc7de3625467473650846ba4e9
ibrahim alzoubi <gusalzib@student.gu.se>
2025-11-27 13:21:47 +0100

    #3 [In Progress] Add login form (html, css)

--- client/harmonai_frontend/src/views/Login.vue
+++ client/harmonai_frontend/src/views/Login.vue
@@ -2,7 +2,12 @@
 
     <div class="login-form-container">
         <div class="login-form">
-            
+            <label for="login_email"> Email </label>
+            <input id="login_email" class="login_input" type="email" placeholder="email">
+            <br>
+            <label for="login_password"> Password </label>
+            <input id="login_password" class="login_input" type="password" placeholder="password">
+            <button type="submit">Login</button>
         </div>
     </div>
 </template>
