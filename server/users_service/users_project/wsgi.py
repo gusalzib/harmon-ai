29784e2e32f8e1d207ffbd992b4c269723babee1
Carl-Johan Erikson <guscarljer@student.gu.se>
2025-11-27 12:06:17 +0100

    #3 Rename and restructure users service

--- server/users_service/users_project/wsgi.py
+++ server/users_service/users_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for harmonai project.
+WSGI config for users_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harmonai.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'users_project.settings')
 
 application = get_wsgi_application()
