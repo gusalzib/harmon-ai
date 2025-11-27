989a63433beea3148fee26504c0721ad8d273e16
ibrahim alzoubi <gusalzib@student.gu.se>
2025-11-27 14:32:20 +0100

    #3 [In Progress] Add signup form (html, css)

--- client/harmonai_frontend/src/views/Signup.vue
+++ client/harmonai_frontend/src/views/Signup.vue
@@ -2,7 +2,14 @@
 
     <div class="signup-form-container">
         <div class="signup-form">
-            
+            <label for="signup_email"> Username </label>
+            <input id="signup_username" class="signup_input" type="text" placeholder="username">
+            <label for="signup_email"> Email </label>
+            <input id="signup_email" class="signup_input" type="email" placeholder="email">
+            <br>
+            <label for="signup_password"> Password </label>
+            <input id="signup_password" class="signup_input" type="password" placeholder="password">
+            <button type="submit">Create Account</button>
         </div>
     </div>
 </template>
