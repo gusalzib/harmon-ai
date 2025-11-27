27d1af45dea8c0a54cc8b279fe2aa9400c540a2f
ibrahim alzoubi <gusalzib@student.gu.se>
2025-11-27 12:36:03 +0100

    #3 [In Progress] Create the Login and Signup vue views files (added them to routes)

---
+++ client/harmonai_frontend/src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <div class="login-form-container">
+        <div class="login-form">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
---
+++ client/harmonai_frontend/src/views/Signup.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <div class="signup-form-container">
+        <div class="signup-form">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
