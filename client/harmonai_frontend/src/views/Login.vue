7ec97ba1864f9d7f2dfe51937882660472699399
ibrahim alzoubi <gusalzib@student.gu.se>
2025-11-27 21:02:15 +0100

    #3 [In Progress] Create login() and toggle() functions in Login.vue

--- client/harmonai_frontend/src/views/Login.vue
+++ client/harmonai_frontend/src/views/Login.vue
@@ -7,6 +7,10 @@
             <br>
             <label for="login_password"> {{ $t('labels.password') }} </label>
             <input id="login_password" class="login_input" type="password" placeholder="password">
+            <div class="show-password-box">
+                <label style="font-size: small; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">{{ $t('labels.ShowPassword') }}</label>
+                <input id="checkbox" type="checkbox" v-on:click="toggle()">
+            </div>
             <button type="submit"> {{ $t('buttons.login') }} </button>
         </div>
     </div>
@@ -17,6 +21,7 @@ import axios from 'axios';
 import { useToast } from 'vue-toastification'
 
 export default {
+    name: 'login',
     data() {
         return {
             form: {
@@ -24,9 +29,9 @@ export default {
                 password: '',
             },
             error: '',
-            url: '',
+            url: 'http://localhost:8000/api/users/login',
             toast: null, // declare a toast variable to be used with toastification library for notifications
-
+            timeout: 2000, // the amount of time to wait before directing the user to home page upon succesful login
                 
             }
     },
@@ -36,14 +41,52 @@ export default {
     },
     methods: {
         async login() {
-                try {
-                    const response = await axios.get(`${this.url}`, {
-                        
-                    })
-                } catch (error) {
-                    
+            try {
+                /* we send the login info as json to backend and await response */
+                const response = await axios.post(`${this.url}`, JSON.stringify(this.form))
+
+                // if the login was successful, we automatically redirect the
+                // user to the home page
+                // timeout is set to 2 seconds as a default
+                if (response.status === 200) {
+
+                    // display notifications
+                    this.toast && this.toast.success(this.$t('notification.loginSuccessful') || 'Login successful');
+
+                    /* this line is commented out for now because we do not have authentication yet
+                        it sets an item called 'token' and store the token received wuth the response */
+                    //localStorage.setItem('token', response.token)
+
+                    // redirect the user to home page
+                        setTimeout(() => {
+                            window.location.replace('/')
+                        }, timeout);
                 }
+                        
+            } catch (error) {
+                    // if password is invalid, display invalid cridentials notification
+                if (error.response?.status === 401) {
+                        // we show a localized notification and if we fail to get that, we have the fallback english string
+                        this.toast && this.toast.error(this.$t('notification.invalidCridentials') || 'Login Failed')
+
+                    // else if email is not found in registry, display user not found notification
+                } else if (error.response?.status === 404) {
+                        // we show a localized notification and if we fail to get that, we have the fallback english string
+                        this.toast && this.toast.error(this.$t('notification.userNotFound') || 'Login Failed')
+                    }
+                }
+        },
+        toggle() {
+            // allow user to show the password in the password filed
+            let temp = document.getElementById("login_password")
+
+            if (temp.type === "password") {
+                temp.type = "text";
+
+            } else {
+                temp.type = "password";
             }
-        }
+            }
+        },
     }
 </script>
