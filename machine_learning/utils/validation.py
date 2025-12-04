59f915a29c32134618df47fedd066c8a225375c1
muhamadj <muhamadj@student.chalmers.se>
2025-12-04 14:53:20 +0100

    #25 [In progress] add validation for bothchroma file containing all 26 columns

--- machine_learning/utils/validation.py
+++ machine_learning/utils/validation.py
@@ -16,3 +16,19 @@ def validate_bothchroma_exist(path_to_file):
         except Exception:
             print(f"Cannot read bothchromas.csv")
             return False
+
+# validating if all the columns are there.
+def validate_bothchroma_columns(path_to_file):
+    bothchroma_file = os.path.join(path_to_file, "bothchroma.csv")
+
+    try:
+        df = pd.read_csv(bothchroma_file, header=None, nrows=1)
+        if len(df.columns) != 26:
+            print(f"File at {bothchroma_file} is missing columns")
+            return False
+        else:
+            return True
+    
+    except Exception:
+        print(f"Cannot read bothchromas.csv")
+        return False
