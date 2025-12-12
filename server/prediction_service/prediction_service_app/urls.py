f35436044a098adf1f64b1f36f5f072bf9508f7b
rebake <rebake@net.chalmers.se>
2025-12-12 15:09:28 +0100

    #4 [in progress] add get songs by genre method

--- server/prediction_service/prediction_service_app/urls.py
+++ server/prediction_service/prediction_service_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("create-song/", views.create_song),
     path("update_song/", views.update_song),
     path("get_specific_song/", views.get_specific_song),
-    path("get_artists_songs/", views.get_artists_songs)
+    path("get_artists_songs/", views.get_artists_songs),
+    path("get_songs_from_genre/",views.get_songs_from_genre),
 ]
--- server/prediction_service/prediction_service_app/views.py
+++ server/prediction_service/prediction_service_app/views.py
@@ -208,6 +208,46 @@ def get_artists_songs(request):
                 "prediction",
             ))
 
+            response = JsonResponse({
+                "songs": songs,
+                'result': 'success',
+            }, status=200)
+            
+        except json.JSONDecodeError:
+            response = JsonResponse({
+                'result': 'error',
+                'message': 'Invalid JSON',
+            },status=400)
+    else:
+        response = JsonResponse({
+            'result': 'error',
+            'message': 'Invalid request method',
+            },status=400)
+        
+    return response
+
+@csrf_exempt
+def get_songs_from_genre(request):
+    if request.method == "GET":
+        try:
+            data = json.loads(request.body)
+            searchGenre = data.get("genre")
+            songs_query= Song.objects.filter(genre=searchGenre)
+            if not songs_query.exists():
+                return JsonResponse({
+                    'result': 'error',
+                    'message': 'No songs found in this genre',
+                    },status=400)
+                
+            #turn result of filter int a song object
+            songs = list(songs_query.values(
+                "title",
+                "artist",
+                "genre",
+                "tempo",
+                "duration",
+                "prediction",
+            ))
 
             response = JsonResponse({
                 "songs": songs,
