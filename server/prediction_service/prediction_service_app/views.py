e2c61e501e58e464cb8557291b0bdd0a6051bc03
Carl-Johan Erikson <guscarljer@student.gu.se>
2025-12-15 20:10:52 +0100

    #22 Temporarily disable saving songs to db

--- server/prediction_service/prediction_service_app/views.py
+++ server/prediction_service/prediction_service_app/views.py
@@ -105,17 +105,17 @@ def create_song(request):
             
             #create the song object and save it to the db
             print("creates song object")
-            new_song = Song.objects.create(
-                title=title,
-                artist=artist,
-                genre=genre,
-                tempo=tempo,
-                duration=duration, 
-                columns=["time","1=C", "2=C#", "3=D", "4=D#", "5=E", "6=F", "7=F#", "8=G", "9=G#", "10=A", "11=A#", "12=B"],
-                chromogram=chroma_T.astype(float).tolist(),
-                prediction=song_chords
-                )
-            new_song.save()
+            #new_song = Song.objects.create(
+            #    title=title,
+            #    artist=artist,
+            #    genre=genre,
+            #    tempo=tempo,
+            #    duration=duration, 
+            #    columns=["time","1=C", "2=C#", "3=D", "4=D#", "5=E", "6=F", "7=F#", "8=G", "9=G#", "10=A", "11=A#", "12=B"],
+            #    #chromogram=chroma_T.astype(float).tolist(),
+            #    prediction=song_chords
+            #    )
+            #new_song.save()
 
     
             response = JsonResponse({
@@ -134,6 +134,8 @@ def create_song(request):
                 'result': 'error',
                 'message': 'Invalid JSON',
             },status=400)
+        except Exception as e:
+            response = JsonResponse({'message': f"Exception: {str(e)}"}, status=500)
     else:
         response = JsonResponse({
             'result': 'error',
