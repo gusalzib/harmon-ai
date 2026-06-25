3b522232da9660ab19387f6e83522d68da273f04
ibrahim alzoubi <gusalzib@student.gu.se>
2026-06-25 18:37:49 +0200

    Clean out large model training files

--- server/validation_training_service/validation_training_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("train/", views.test_train_model, name="test_train_model"),
-    path("report/", views.fetch_reports, name="fetch_reports"),
-    path("update/", views.update_model_name, name="update_model_name")
-
-]
